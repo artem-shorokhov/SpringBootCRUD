8b7cdcc2fd8911648dc4062d4058d98dadd9d650
Artem Shorokhov <artem.shorokhov@gmail.com>
2017-05-04 12:14:32 +0300

    Multiple CrudService implementations.

--- src/main/java/org/enigma/crud/controller/CrudController.java
+++ src/main/java/org/enigma/crud/controller/CrudController.java
@@ -2,6 +2,7 @@ package org.enigma.crud.controller;
 
 import org.enigma.crud.model.Table;
 import org.enigma.crud.util.CrudService;
+import org.enigma.crud.util.JdbcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
